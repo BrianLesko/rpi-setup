695f5ad76869ff03d35abd64b9778b32ac5f58b1
Brian Lesko <brianjosephlesko@gmail.com>
2024-05-20 19:34:13 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 username=lesko
+email=my_email
 
 sudo apt-get update -y && sudo apt upgrade -y 
 
@@ -12,7 +13,8 @@ sudo apt-get install openssh-server -y
 
 sudo apt-get install -y git
 
-sudo apt remove brltty
+sudo apt remove brltty -y
+sudo apt thunderbird -y
 
 sudo usermod -a -G video $username
 sudo usermod -a -G dialout $username
@@ -20,3 +22,14 @@ sudo usermod -a -G dialout $username
 # Print hostname and IP address
 echo "Hostname: $(hostname)"
 echo "IP Address: $(hostname -I)"
+
+# ssh keygen for github access, generate the key on your pi 
+ssh-keygen -t ed25519 -C $username
+
+# copy the key to github ssh keys 
+
+# copy the key to your personal machine 
+ssh-copy-id -i ~/.ssh/id_rsa.pub $username@remote-host
+
+
+
